9f0d777e025d5d7899bf37319b404cb77fcb2052
MC_XiaoHei <xiaohei.xor7@outlook.com>
2024-07-11 15:37:11 +0800

    chore: move now use

--- src/main/java/iafenvoy/accountswitcher/mixins/TitleScreenMixin.java
+++ src/main/java/iafenvoy/accountswitcher/mixins/TitleScreenMixin.java
@@ -46,7 +46,7 @@ public class TitleScreenMixin extends Screen {
         float f = this.doBackgroundFade ? (float) (Util.getMeasuringTimeMs() - this.backgroundFadeStart) / 1000.0F : 1.0F;
         float g = this.doBackgroundFade ? MathHelper.clamp(f - 1.0F, 0.0F, 1.0F) : 1.0F;
         int l = MathHelper.ceil(g * 255.0F) << 24;
-        context.drawCenteredTextWithShadow(this.textRenderer, AccountManager.getAccountInfoText(), this.width / 2, this.height - 50, 16777215 | l);
+        context.drawCenteredTextWithShadow(this.textRenderer, AccountManager.getAccountInfoText(), this.width / 2, this.height - 58, 16777215 | l);
     }
 
     @Unique
